9f9a947497b9c2293202da5a078607f9ac467b36
norizwana <ezwana19@gmail.com>
2023-03-17 11:33:47 +0800

    add PHPMailer

--- forms/contact.php
+++ forms/contact.php
@@ -1,10 +1,14 @@
 <?php
+// error_reporting(E_ALL);
+// ini_set('display_errors', 1);
+
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $name = $_POST["name"];
     $clinic = $_POST["clinic"];
     $email = $_POST["email"];
     $subject = $_POST["subject"];
-    $message = $_POST["message"];
+    $inquiries = $_POST["inquiries"];
 
     $to = "ezwana19@gmail.com";
     $subject = "New Message from $name via the website";
@@ -22,5 +26,39 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     http_response_code(403);
     echo "There was a problem with your submission, please try again.";
 }
+
+// $to = 'norizaezwanak@gmail.com'; // Put in your email address here
+// $subject  = "Contact Us Form"; // The default subject. Will appear by default in all messages. Change this if you want.
+
+// // User info (DO NOT EDIT!)
+// $name = stripslashes($_REQUEST['name']); // sender's name
+// $clinic = stripslashes($_REQUEST['clinic']); // sender's name
+// $email = stripslashes($_REQUEST['email']); // sender's email
+// $phone = stripslashes($_REQUEST['phone']); 
+// $inquiries = stripslashes($_REQUEST['inquiries']);
+
+
+
+// // The message you will receive in your mailbox
+// // Each parts are commented to help you understand what it does exaclty.
+// // YOU DON'T NEED TO EDIT IT BELOW BUT IF YOU DO, DO IT WITH CAUTION!
+// $msg .= "Name: ".$name."\r\n\n";  // add sender's name to the message
+// $msg .= "Clinic: ".$clinic."\r\n\n";  // add sender's name to the message
+// $msg .= "E-mail: ".$email."\r\n\n";  // add sender's email to the message
+// $msg .= "Phone: ".$phone."\r\n\n";  // add sender's phone to the message
+// $msg .= "inquiries: ".$inquiries."\r\n\n";  // add sender's checkboxes to the message
+// $msg .= "\r\n\n"; 
+
+// $mail = @mail($to, $subject, $msg, "From:".$email);  // This command sends the e-mail to the e-mail address contained in the $to variable
+
+// if($mail) {
+// 	header("Location:index.html");	
+// } else {
+// 	echo 'Message could not be sent!';   //This is the message that will be shown when an error occured: the message was not send
+// }
+
+
 ?>
+
+
 

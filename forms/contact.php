d0a0a798de63da95e7eabea113f61bdb0eb0c883
norizwana <ezwana19@gmail.com>
2023-03-20 08:07:32 +0800

    progress

--- forms/contact.php
+++ forms/contact.php
@@ -43,35 +43,6 @@ if(isset($_POST['name']) && isset($_POST['email'])){
 
 }
 
-
-// error_reporting(E_ALL);
-// ini_set('display_errors', 1);
-
-
-// if ($_SERVER["REQUEST_METHOD"] == "POST") {
-//     $name = $_POST["name"];
-//     $clinic = $_POST["clinic"];
-//     $email = $_POST["email"];
-//     $phone = $_POST["phone"];
-//     $message= $_POST["message"];
-
-//     $to = "ezwana19@gmail.com";
-//     $subject = "New Message from $name via the website";
-//     $body = "Name: $name\nClinic: $clinic\nEmail: $email\n\nMessage:\n$message";
-//     $headers = "From: $email";
-
-//     if (mail($to, $subject, $body, $headers)) {
-//         http_response_code(200);
-//         echo "Thank you! Your message has been sent.";
-//     } else {
-//         http_response_code(500);
-//         echo "Oops! Something went wrong and we couldn't send your message.";
-//     }
-// } else {
-//     http_response_code(403);
-//     echo "There was a problem with your submission, please try again.";
-// }
-
 ?>
 
 
